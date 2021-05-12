68ac3912b3a5cdb1d0673ab5f5849b9dd37562fa
Elsaeed Ahmed <elsaeedahmed97@gmail.com>
2021-05-12 17:57:36 +0200

    fix pytest pipeline

--- san3a/users/tests/test_drf_views.py
+++ san3a/users/tests/test_drf_views.py
@@ -1,7 +1,8 @@
 import pytest
 from django.test import RequestFactory
-from zekra.users.api.views import UserViewSet
-from zekra.users.models import User
+
+from san3a.users.api.views import UserViewSet
+from san3a.users.models import User
 
 pytestmark = pytest.mark.django_db
 
